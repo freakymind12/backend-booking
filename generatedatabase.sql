2f0454c386ada9ae74573e77c8728087321dd73e
freakymind12 <riankurniawan132@gmail.com>
2024-10-15 08:16:30 +0700

    update .sql file for generating db

--- generatedatabase.sql
+++ generatedatabase.sql
@@ -1,110 +1,135 @@
-CREATE TABLE `final_status`(
-    `id_final_status` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `id_status` INT UNSIGNED NOT NULL,
-    `id_machine` VARCHAR(255) NOT NULL,
-    `id_problem` INT UNSIGNED NOT NULL,
-    `start` DATETIME NOT NULL,
-    `end` DATETIME NOT NULL
-);
-CREATE TABLE `production`(
-    `id_production` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `date` DATETIME NOT NULL,
-    `shift` INT NOT NULL,
-    `id_pca` INT UNSIGNED NOT NULL,
-    `ok` BIGINT NOT NULL,
-    `ng` BIGINT NOT NULL,
-    `reject_setting` BIGINT NOT NULL,
-    `production_time` BIGINT NOT NULL,
-    `stop_time` BIGINT NOT NULL,
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` DATETIME NULL
-);
-CREATE TABLE `kanagata`(
-    `id_kanagata` VARCHAR(255) NOT NULL,
-    `actual_shot` BIGINT DEFAULT 0 NOT NULL,
-    `limit_shot` BIGINT DEFAULT 0 NOT NULL,
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` DATETIME NULL
-);
-ALTER TABLE
-    `kanagata` ADD PRIMARY KEY(`id_kanagata`);
-CREATE TABLE `users`(
-    `id_user` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `username` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `roles` VARCHAR(255) NOT NULL DEFAULT 'viewer',
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` DATETIME NULL
-);
-CREATE TABLE `pca`(
-    `id_pca` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `id_machine` VARCHAR(255) NOT NULL,
-    `id_product` VARCHAR(255) NOT NULL,
-    `id_kanagata` VARCHAR(255) NOT NULL,
-    `speed` INT NOT NULL,
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` DATETIME NULL
-);
-CREATE TABLE `product`(
-    `id_product` VARCHAR(255) NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` DATETIME NULL
-);
-ALTER TABLE
-    `product` ADD PRIMARY KEY(`id_product`);
-CREATE TABLE `machine`(
-    `id_machine` VARCHAR(255) NOT NULL,
-    `actual_shot` BIGINT NOT NULL,
-    `limit_shot` BIGINT NOT NULL,
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `address` VARCHAR(255) NOT NULL,
-    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` DATETIME NULL
-);
-ALTER TABLE
-    `machine` ADD PRIMARY KEY(`id_machine`);
-CREATE TABLE `status`(
-    `id_status` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL
-);
-CREATE TABLE `log_status`(
-    `id_log_status` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `id_problem` INT UNSIGNED NOT NULL,
-    `id_status` INT UNSIGNED NOT NULL,
-    `id_machine` VARCHAR(255) NOT NULL,
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-CREATE TABLE `problem`(
-    `id_problem` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL,
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` DATETIME NULL
-);
-ALTER TABLE
-    `final_status` ADD CONSTRAINT `final_status_id_status_foreign` FOREIGN KEY(`id_status`) REFERENCES `status`(`id_status`);
-ALTER TABLE
-    `pca` ADD CONSTRAINT `pca_id_product_foreign` FOREIGN KEY(`id_product`) REFERENCES `product`(`id_product`);
-ALTER TABLE
-    `production` ADD CONSTRAINT `production_id_pca_foreign` FOREIGN KEY(`id_pca`) REFERENCES `pca`(`id_pca`);
-ALTER TABLE
-    `final_status` ADD CONSTRAINT `final_status_id_machine_foreign` FOREIGN KEY(`id_machine`) REFERENCES `machine`(`id_machine`);
-ALTER TABLE
-    `pca` ADD CONSTRAINT `pca_id_kanagata_foreign` FOREIGN KEY(`id_kanagata`) REFERENCES `kanagata`(`id_kanagata`);
-ALTER TABLE
-    `log_status` ADD CONSTRAINT `log_status_id_problem_foreign` FOREIGN KEY(`id_problem`) REFERENCES `problem`(`id_problem`);
-ALTER TABLE
-    `final_status` ADD CONSTRAINT `final_status_id_problem_foreign` FOREIGN KEY(`id_problem`) REFERENCES `problem`(`id_problem`);
-ALTER TABLE
-    `pca` ADD CONSTRAINT `pca_id_machine_foreign` FOREIGN KEY(`id_machine`) REFERENCES `machine`(`id_machine`);
-ALTER TABLE
-    `log_status` ADD CONSTRAINT `log_status_id_machine_foreign` FOREIGN KEY(`id_machine`) REFERENCES `machine`(`id_machine`);
-ALTER TABLE
-    `log_status` ADD CONSTRAINT `log_status_id_status_foreign` FOREIGN KEY(`id_status`) REFERENCES `status`(`id_status`);
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Okt 2024 pada 03.14
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 7.3.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `roomdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `id_booking` varchar(20) NOT NULL,
+  `id_user` int(10) UNSIGNED NOT NULL,
+  `id_room` varchar(20) NOT NULL,
+  `meeting_name` text NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL,
+  `status` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id_room` varchar(20) NOT NULL,
+  `room_name` varchar(255) NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `rooms`
+--
+
+INSERT INTO `rooms` (`id_room`, `room_name`, `capacity`, `description`) VALUES
+('2f2a3027-7aeb-11ef-9', 'Meeting Room 1', 5, 'This room is for executive meeting with many equipment '),
+('34782b89-7aeb-11ef-9', 'Meeting Room 2', 10, 'This room is suitable for small meeting with customers or vendors'),
+('51e2fae3-7aeb-11ef-9', 'Meeting Room 3', 20, 'This meeting room is for departement meeting. Equipped with internet network and sound');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(10) UNSIGNED NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `roles` varchar(255) NOT NULL DEFAULT 'viewer',
+  `dept` varchar(10) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id_user`, `username`, `password`, `email`, `roles`, `dept`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Rian Kurniawan', '$2b$10$TLIR1dP7hX3ce//CfVc9ae97XlN/Cg1UlFt/8kmYXJ5sLp8JhKkLG', 'rian.kurniawan.6n@hirose-gl.com', 'admin', 'PE', '2024-03-06 13:45:37', '2024-10-08 06:23:17', NULL),
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id_booking`),
+  ADD KEY `booking_foreign_user` (`id_user`),
+  ADD KEY `booking_foreign_room` (`id_room`);
+
+--
+-- Indeks untuk tabel `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id_room`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `booking_foreign_room` FOREIGN KEY (`id_room`) REFERENCES `rooms` (`id_room`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `booking_foreign_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
