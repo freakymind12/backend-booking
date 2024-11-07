02c8860f1ef817807e4a9c56564bbc1c45efafa9
freakymind12 <riankurniawan132@gmail.com>
2024-11-07 09:43:34 +0700

    add docker configuration & remove apicache

--- src/app.js
+++ src/app.js
@@ -3,14 +3,11 @@ const PORT = process.env.PORT || 5000
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const cron = require('node-cron')
-// const apicache = require('apicache')
 
 // IMPORT SCHEDULER
 const { startScheduler } = require('./scheduler/deleteExpiredBooking.js')
 
 startScheduler()
-// const bookingScheduler = require('./controllers/bookingController.js')
 
 // IMPORT ROUTES
 const authRoutes = require('./routes/auth.js');
@@ -20,7 +17,6 @@ const bookingRoutes = require('./routes/booking.js')
 
 // IMPORT MIDDLEWARE
 const middlewareHandle = require('./middleware/middlewareHandle.js');
-// const cache = apicache.middleware;
 const app = express();
 
 // MIDDLEWARE USED
@@ -28,22 +24,11 @@ app.use(middlewareHandle.errorMessage) // error message
 app.use(middlewareHandle.logRequest) // log request
 // app.use(middlewareHandle.allowCrossDomain) // allow cross domain for make a request to this api
 app.use(cors());
-// app.use(cache("2 minutes"));
 
 // ALLOW JSON RESPONSE AND PUBLIC FOLDER
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.static('public'));
-
-// TASK SCHEDULER
-// cron.schedule('*/10 * * * * *', () => {
-  // const now = new Date();
-  // const formattedTime = now.toLocaleTimeString();
-  // bookingScheduler.deleteExpiredBooking()
-  // console.log(`[${formattedTime}] -> running a task by node-cron`);
-// });
-    
-
 
 // ROUTES
 app.use('/auth', authRoutes) // auth login ✔
