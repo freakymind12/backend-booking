600336939aa219b61f7ef20b23975c5e32099628
freakymind12 <riankurniawan132@gmail.com>
2025-05-15 15:14:08 +0700

    update auto booking service from hardcode schedule to get schedule from autoschedule model

--- src/services/autoBooking.js
+++ src/services/autoBooking.js
@@ -1,53 +1,66 @@
 const cron = require("node-cron")
 const bookingModel = require("../models/bookings")
+const autoScheduleModel = require("../models/auto_schedule")
 const dayjs = require("dayjs")
 
-const schedule = [
-  {
-    id_room: '1d91ade8-219e-11f0-8cbf-066f82d8bae1',
-    day: 'Monday',
-    id_user: 2,
-    meeting_name: "Leader Meeting",
-    start: '13:00:00',
-    end: "14:00:00"
-  },
-  {
-    id_room: '1d91ade8-219e-11f0-8cbf-066f82d8bae1',
-    day: 'Wednesday',
-    id_user: 2,
-    meeting_name: "Production Meeting",
-    start: '13:00:00',
-    end: "14:00:00"
-  }
-]
+const dayNameToNumber = {
+  Sunday: 0,
+  Monday: 1,
+  Tuesday: 2,
+  Wednesday: 3,
+  Thursday: 4,
+  Friday: 5,
+  Saturday: 6,
+};
 
 const autoBookingSchedule = () => {
-  cron.schedule("0 6 * * *", async () => {
+  cron.schedule("0 5 * * *", async () => {
     try {
-      const today = dayjs().format('dddd')
+      const today = dayjs(); // hari ini (Jumat)
+
+      const schedule = await autoScheduleModel.get();
+
       for (const item of schedule) {
-        if (item.day === today) {
-          const checkBooking = await bookingModel.checkBooking({
-            id_room: item.id_room,
-            start: dayjs().format('YYYY-MM-DD') + ' ' + item.start,
-            end: dayjs().format('YYYY-MM-DD') + ' ' + item.end,
+
+        const { day, start, end, id_room, id_user, meeting_name } = item
+        const targetDayNumber = dayNameToNumber[day];
+        if (targetDayNumber === undefined) {
+          console.warn(`Hari tidak valid: ${day}`);
+          continue;
+        }
+        // Hitung tanggal hari target di minggu depan dari hari ini
+        // Hari ini hari Jumat (day = 5)
+        const daysUntilTarget = (targetDayNumber + 7 - today.day()) % 7 || 7;
+        // penjelasan: kalau hasilnya 0 berarti hari ini, kita ganti jadi 7 agar minggu depan
+        const targetDate = today.add(daysUntilTarget, 'day').format('YYYY-MM-DD');
+
+        const formattedStart = targetDate + ' ' + start;
+        const formattedEnd = targetDate + ' ' + end;
+
+        const checkBooking = await bookingModel.checkBooking({
+          id_room,
+          start: formattedStart,
+          end: formattedEnd,
+        })
+        if (checkBooking.length === 0) {
+          await bookingModel.create({
+            id_user,
+            id_room,
+            meeting_name,
+            start: formattedStart,
+            end: formattedEnd,
           })
 
-          if (checkBooking.length === 0) {
-            await bookingModel.create({
-              ...item,
-              start: dayjs().format('YYYY-MM-DD') + ' ' + item.start,
-              end: dayjs().format('YYYY-MM-DD') + ' ' + item.end,
-            })
-
-            console.log(`[SCHEDULER] - SUCCESS ADD BOOKING FOR ${item.meeting_name} on ${dayjs().format('YYYY-MM-DD')} at ${item.start} - ${item.end}`)
-          }
+          console.log(`[SCHEDULER] - SUCCESS ADD BOOKING FOR ${meeting_name} on ${targetDate} at ${start} - ${end}`)
+        }
+        else {
+          console.log(`[SCHEDULER] - BOOKING FOR ${meeting_name} on ${targetDate} at ${start} - ${end} ALREADY EXIST`)
         }
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  })
+  });
 }
 
 module.exports = autoBookingSchedule
